9090df62e34e41987e4b6b372b6c25a9576e705b
Adam Bauer -- Chicago Computer <ambauer@uchicago.edu>
2026-01-08 14:08:52 -0600

    feat: cmip fitting script with descriptive print messages

--- main_cmip_fitting.py
+++ main_cmip_fitting.py
@@ -7,7 +7,9 @@ Jan 2026
 To run: main_gev_fit.py GRID STAT
 """
 
+import os
 import sys
+import shutil
 
 import xarray as xr
 
@@ -15,36 +17,56 @@ from config import DATA_ROOT
 from src.mle import ds_mle_fit
 
 # import command line grid
-GRID, STAT = sys.argv[1:]
+STAT, = sys.argv[1:]
+width = shutil.get_terminal_size(fallback=(80, 20)).columns
 
-print("Importing land-masked data...")
 # define variables and open datasets
-vars = ['t2m_annual_max', 't2m_annual_min']
-fnames = ['era5_' + VAR + '_' + GRID + '_landonly.nc' for VAR in vars]
-dss = [xr.open_dataset(DATA_ROOT / 'ERA5' /'landonly' / fname) for fname in fnames]
+vars = ['tas_annual_max', 'tas_annual_min']
 
-# carry out GEV fitting for each dataset
-if STAT == 'stat':
-    print("Doing stationary GEV fits...")
-    dss_with_fit = [ds_mle_fit(ds, var_name='t2m') for ds in dss]
-    dss_with_fit_on_both = [ds_mle_fit(ds, var_name='t2m_anom') for ds in dss_with_fit]
-
-elif STAT == 'nonstat':
-    print("Doing nonstationary GEV fits...")
-    dss_with_fit = [ds_mle_fit(ds, var_name='t2m',
-                               fit_dim='year', non_stat=True) for ds in dss]
-    dss_with_fit_on_both = [ds_mle_fit(ds, var_name='t2m_anom',
-                                       fit_dim='year', non_stat=True) for ds in dss_with_fit]
-
-else:
-    raise ValueError("Invalid entry for command line argument `STAT` (supports 'stat' or 'nonstat').")
+for var in vars:
+    print('='*width)
+    print("🏋🏼‍♀️ Carrying out GEV fits for: ", var)
+    print('='*width)
     
-print("GEV fitting complete.")
+    # make data directory if they don't exist
+    print("🧐 Making a {} directory if it doesn't exist...".format(DATA_ROOT / 'CMIP6' / var / 'gev'))
+    os.makedirs(DATA_ROOT / 'CMIP6' / var / 'gev', exist_ok=True)
+    data_path = DATA_ROOT / 'CMIP6' / var
 
-print("Saving datasets...")
-# save datasets
-for VAR, ds_masked in zip(vars, dss_with_fit_on_both):
-    ds_masked.to_netcdf(DATA_ROOT / 'ERA5' / 'landonly' / ('era5_' + VAR + '_' + GRID + '_landonly_gev_'
-                        + STAT + '.nc'))
+    # make all landonly file names
+    fnames = [f for f in data_path.glob("*_landonly.nc")]
 
-print("Datasets saved to f{DATA_ROOT}/ERA5/landonly/")
+    # open dataset
+    print('-'*width)
+    print("🪏 Importing land-masked data...")
+    # dss = [xr.open_dataset(DATA_ROOT / 'ERA5' /'landonly' / fname) for fname in fnames]
+
+    # carry out GEV fitting for each dataset
+    if STAT == 'stat':
+        print('-'*width)
+        print("🧮 Doing stationary GEV fits...")
+        #dss_with_fit = [ds_mle_fit(ds, var_name='t2m') for ds in dss]
+        #dss_with_fit_on_both = [ds_mle_fit(ds, var_name='t2m_anom') for ds in dss_with_fit]
+
+    elif STAT == 'nonstat':
+        print('-'*width)
+        print("🧮 Doing nonstationary GEV fits...")
+        #dss_with_fit = [ds_mle_fit(ds, var_name='t2m',
+        #                        fit_dim='year', non_stat=True) for ds in dss]
+        #dss_with_fit_on_both = [ds_mle_fit(ds, var_name='t2m_anom',
+        #                                fit_dim='year', non_stat=True) for ds in dss_with_fit]
+
+    else:
+        raise ValueError("Invalid entry for command line argument `STAT` (supports 'stat' or 'nonstat').")
+    
+    print('-'*width)
+    print("✅ GEV fitting complete.")
+
+    print('-'*width)
+    print("✍️ Saving datasets...")
+    # save datasets
+    #for VAR, ds_masked, fname in zip(vars, dss_with_fit_on_both, fnames):
+    #    ds_masked.to_netcdf(DATA_ROOT / 'CMIP6' / VAR / 'gev' / (fname[:-3] + '_gev_' + STAT + '.nc'))
+    
+    print('-'*width)
+    print("✅ Datasets saved to f{DATA_ROOT}/ERA5/landonly/")
