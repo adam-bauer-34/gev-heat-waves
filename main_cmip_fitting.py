39f23ce18078b92b5933e9b17449f772f2d1c45c
Adam Bauer -- Chicago Computer <ambauer@uchicago.edu>
2026-01-08 11:49:36 -0600

    refac: clarified preprocessing script name

---
+++ main_cmip_fitting.py
@@ -0,0 +1,50 @@
+"""Main file for GEV fitting of CMIP data.
+
+Adam Michael Bauer
+UChicago
+Jan 2026
+
+To run: main_gev_fit.py GRID STAT
+"""
+
+import sys
+
+import xarray as xr
+
+from config import DATA_ROOT
+from src.mle import ds_mle_fit
+
+# import command line grid
+GRID, STAT = sys.argv[1:]
+
+print("Importing land-masked data...")
+# define variables and open datasets
+vars = ['t2m_annual_max', 't2m_annual_min']
+fnames = ['era5_' + VAR + '_' + GRID + '_landonly.nc' for VAR in vars]
+dss = [xr.open_dataset(DATA_ROOT / 'ERA5' /'landonly' / fname) for fname in fnames]
+
+# carry out GEV fitting for each dataset
+if STAT == 'stat':
+    print("Doing stationary GEV fits...")
+    dss_with_fit = [ds_mle_fit(ds, var_name='t2m') for ds in dss]
+    dss_with_fit_on_both = [ds_mle_fit(ds, var_name='t2m_anom') for ds in dss_with_fit]
+
+elif STAT == 'nonstat':
+    print("Doing nonstationary GEV fits...")
+    dss_with_fit = [ds_mle_fit(ds, var_name='t2m',
+                               fit_dim='year', non_stat=True) for ds in dss]
+    dss_with_fit_on_both = [ds_mle_fit(ds, var_name='t2m_anom',
+                                       fit_dim='year', non_stat=True) for ds in dss_with_fit]
+
+else:
+    raise ValueError("Invalid entry for command line argument `STAT` (supports 'stat' or 'nonstat').")
+    
+print("GEV fitting complete.")
+
+print("Saving datasets...")
+# save datasets
+for VAR, ds_masked in zip(vars, dss_with_fit_on_both):
+    ds_masked.to_netcdf(DATA_ROOT / 'ERA5' / 'landonly' / ('era5_' + VAR + '_' + GRID + '_landonly_gev_'
+                        + STAT + '.nc'))
+
+print("Datasets saved to f{DATA_ROOT}/ERA5/landonly/")
