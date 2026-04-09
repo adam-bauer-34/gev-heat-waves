13814feac0202ee54e2f1fb94f623a932a0422d0
Adam Michael Bauer <ambauer@uchicago.edu>
2026-04-09 12:22:52 -0500

    quick fix to FIT_REGISTRY

--- src/evt_heat_waves/mip_fit/__init__.py
+++ src/evt_heat_waves/mip_fit/__init__.py
@@ -1,35 +1,16 @@
-from .cmip.most.most import runner as cmip_most_runner
-from .cmip.most.most_mpi import runner as cmip_most_mpi_runner
+from .most.most import runner as most_runner
+from .most.most_mpi import runner as most_mpi_runner
 
-from .cmip.prim.prim import runner as cmip_prim_runner
-from .cmip.prim.prim_mpi import runner as cmip_prim_mpi_runner
-
-from .amip.most.most import runner as amip_most_runner
-from .amip.most.most_mpi import runner as amip_most_mpi_runner
-
-from .amip.prim.prim import runner as amip_prim_runner
-from .amip.prim.prim_mpi import runner as amip_prim_mpi_runner
-
+from .prim.prim import runner as prim_runner
+from .prim.prim_mpi import runner as prim_mpi_runner
 
 FIT_REGISTRY = {
-    'cmip': {
-        'most': {
-            'no_mpi': cmip_most_runner,
-            'mpi': cmip_most_mpi_runner
-        },
-        'prim': {
-            'no_mpi': cmip_prim_runner,
-            'mpi': cmip_prim_mpi_runner
-        },
+    'most': {
+        'no_mpi': most_runner,
+        'mpi': most_mpi_runner
     },
-    'amip': {
-        'most': {
-            'no_mpi': amip_most_runner,
-            'mpi': amip_most_mpi_runner
-        },
-        'prim': {
-            'no_mpi': amip_prim_runner,
-            'mpi': amip_prim_mpi_runner
-        },
+    'prim': {
+        'no_mpi': prim_runner,
+        'mpi': prim_mpi_runner
     }
 }
