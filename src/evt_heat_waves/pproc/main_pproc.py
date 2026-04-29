1ff280e8d74cde88d90f21dfe46e10ecb6a8b0ce
Adam Michael Bauer <ambauer@uchicago.edu>
2026-04-29 11:23:20 -0500

    main file reorg, now each data source folder holds mains

--- src/evt_heat_waves/pproc/main_pproc.py
+++ src/evt_heat_waves/pproc/main_pproc.py
@@ -8,7 +8,7 @@ Apr 8, 2026
 import time
 import shutil
 
-from evt_heat_waves.cli import parse_args_pproc
+from evt_heat_waves.pproc.cli import parse_args_pproc
 from evt_heat_waves.logging_utils import setup_logger, get_git_hash
 from evt_heat_waves.pproc import PPROC_REGISTRY
 
