c465a397000c2545e0dfa9f0880f6d1dcc9d7d8c
Adam Michael Bauer <ambauer@uchicago.edu>
2026-05-11 18:53:13 -0500

    testing done for stat_new

--- src/evt_heat_waves/mle/__init__.py
+++ src/evt_heat_waves/mle/__init__.py
@@ -8,6 +8,7 @@ from .hess import get_hessian_stat_fix_shape
 # define registry of valid hessians to use to calculate standard errors of MLE
 HESS_REGISTRY = {
     'stat': get_hessian_stat,
+    'stat_new': get_hessian_stat,
     'nonstat': get_hessian_nonstat,
     'stat_fix_shape': get_hessian_stat_fix_shape
 }
